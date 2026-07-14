899dffa7b22fd4b4846f1f79882be3a9b3821f18
amaysagarvp <amaysagar60@gmail.com>
2026-07-14 12:13:42 +0530

    new changes

--- scripts.js
+++ scripts.js
@@ -3,42 +3,42 @@ const sBtn = document.getElementById("searchBtn");
 const weather = document.getElementById("weather");
 
 sBtn.addEventListener("click", () => {
-    console.log("Button clicked");
-    const cityName = input.value.trim();
+    const city = input.value.trim();
 
-    if (cityName === "") {
+    searchWeather(city);
+});
+function searchWeather(city) {
+    if (city == "") {
         return;
     }
+    showLoading();
+    fetchWeather(city);
+}
+function showLoading() {
+    weather.innerHTML = `<h2>Loading...</h2>`;
+}
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9c1fb7c8d10ff4f4216554f28e97463d`;
-    weather.innerHTML = `<h2>loading</h2>`
+async function fetchWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9c1fb7c8d10ff4f4216554f28e97463d`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayWeather(data);
+    } catch (error) {
+        showError("Network error...");
+    }
+}
+    function showError(message) {
+        weather.innerHTML = `<h2>${message}</h2>`;
+    }
+    function displayWeather(data){
+       const celsius = data.main.temp - 273.15;
 
+       const condition = data.weather[0].description;
 
-
-    fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
- 
-            if (data.cod !== 200) {
-
-                weather.innerHTML =
-                    `<h2>City is not found</h2>`;
-                return;
-            }
-
-             const icon = data.weather[0].icon;
-            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
-            const temperature = data.main.temp - 273.15;
-
-            weather.innerHTML = `          
-<h2>${data.name}</h2>
-<img src="${iconUrl}" alt="Weather Icon">
-<p>Temperature: ${temperature.toFixed(1)} °C</p>
-<p>Wind Speed: ${data.wind.speed} m/s</p>
-`;
-        });
-});
-
+       weather.innerHTML = `
+       <h2>${data.name}</h2>
+       <p class="temp">Temperature: <strong>${celsius.toFixed(1)} °C</strong></p>
+        <p>Condition: <span>${condition}</span></p>
+        <p>Wind Speed: <span>${data.wind.speed} m/s</span></p>`;
+    }
